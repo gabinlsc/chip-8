6cd5a7f8304e697efa400c6feb1e51bb0a36b069
Gabin LESCOAT <lescoatgabin@gmail.com>
2026-09-25 09:03:43 +0000

    Build du programme

---
+++ build/CMakeFiles/chip8_emu.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chip8_emu.dir/src/chip8.cpp.o"
+  "CMakeFiles/chip8_emu.dir/src/chip8.cpp.o.d"
+  "CMakeFiles/chip8_emu.dir/src/main.cpp.o"
+  "CMakeFiles/chip8_emu.dir/src/main.cpp.o.d"
+  "chip8_emu"
+  "chip8_emu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chip8_emu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
